9f11d767a37b0dbfda7da7576f00f8aeeb4faeb1
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-03 20:57:44 +0000

    Add task 1:  Install a package. Task 2: Execute a command

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,4 @@
+# 1. Install a package
+package { 'puppet-lint':
+    ensure => installed,
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# 2. Execute a command
+exec { 'killmenow':
+    path => '/usr/bin',
+    provider => 'shell',
+    command => 'pkill -f ./killmenow',
+}
