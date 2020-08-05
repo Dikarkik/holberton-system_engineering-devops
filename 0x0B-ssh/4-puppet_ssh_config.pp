18e8d5d9ac0ba727be268aa826e0d2c71a148c19
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-05 04:02:04 +0000

    Add task 4: Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# make changes to our configuration file
+# configured to use the private key ~/.ssh/holberton
+# configured to refuse to authenticate using a password
+
+file_line { 'conf PasswordAuthentication':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+    match  => '^PasswordAuthentication'
+}
+
+file_line { 'conf IdentityFile' :
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
+    match  => '^IdentityFile',
+}
