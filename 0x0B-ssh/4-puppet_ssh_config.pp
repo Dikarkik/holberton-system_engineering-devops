62ce08dcbf17bdc400e783566742f19e8c95abef
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-05 04:02:32 +0000

    Add task 4: Client configuration file (w/ Puppet)

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# 4. Client configuration file (w/ Puppet)
 # make changes to our configuration file
 # configured to use the private key ~/.ssh/holberton
 # configured to refuse to authenticate using a password
